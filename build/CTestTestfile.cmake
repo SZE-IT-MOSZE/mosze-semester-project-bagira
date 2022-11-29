72c62f1179f1163c4fa36b8f47c0df22f10f1786
Erik Zsolt Skobrák <63049737+skobizsolt@users.noreply.github.com>
2022-11-29 21:00:00 +0100

    fix: unstage build directory from github
    Reason: these files are not allowed to be pushed, it's set to be gitignored in a future commit

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Gergo/Desktop/mosze/mosze-semester-project-bagira
-# Build directory: C:/Users/Gergo/Desktop/mosze/mosze-semester-project-bagira/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Gergo/Desktop/mosze/mosze-semester-project-bagira/build/demo-test[1]_include.cmake")
-subdirs("_deps/googletest-build")
