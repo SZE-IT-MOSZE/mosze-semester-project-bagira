79c6393341fc99798c92f334152b0e0449e36468
zsxlti <zsoltibaloghvp@gmail.com>
2022-12-15 18:18:56 +0100

    Added toPage attribute to Options in Story.json

--- Test.cpp
+++ Test.cpp
@@ -4,17 +4,11 @@
 #include "include/Page.cpp"
 #include "include/Page.h"
 
-// Demonstrate some basic assertions.
-TEST(BasicAssertions, CheckIfEmpty) {
-  Option option("", 1, 20);
-  // Expect answer to be "Valami" and not to be null
-  EXPECT_EQ(option.getAnswer(), "Valami");
-  EXPECT_NE(option.getAnswer(), NULL);
-}
+// Demonstrate some basic assertions
 
 TEST(BasicAssertions, CheckData) {
   Option option("Első", 10, 13);
-   Option option2("Második", 5, 20);
+  Option option2("Második", 5, 20);
   std::vector<Option> options;
   options.push_back(option);
   options.push_back(option2);
@@ -22,3 +16,15 @@ TEST(BasicAssertions, CheckData) {
   // Expect page size to be 2
   EXPECT_EQ(testPage.getOptionSize(), 2);
 }
+
+TEST(BasicAssertions, CheckIfEmpty) {
+  Option option("", 1, 20);
+  // Expect answer to be "Valami" and not to be null
+  EXPECT_EQ(option.getAnswer(), "Valami");
+  EXPECT_NE(option.getAnswer(), NULL);
+}
+
+// int main(int argc, char **argv) {
+//     ::testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
